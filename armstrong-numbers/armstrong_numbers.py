d8bb68398c47d2edee815b084ac9807acd431030
Drahiri <kodzidg@o2.pl>
2025-11-06 18:07:49 +0100

    Define function 'is_armstrong_number'

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,12 @@
 def is_armstrong_number(number: int) -> bool:
-    pass
+    """Checks if number is Armstrong number.
+
+    :param number: int - number to test.
+    :return: bool - true when number is Armstrong number.
+
+    Armstrong number is a number that is the sum of its own digits each raised
+    to the power of the number of digits.
+    """
+
+    str_num = str(number)
+    return number == sum([int(x) ** len(str_num) for x in str_num])
