0aabdc464ba8f4a5f7da2bc7bf130445e880db91
Drahiri <kodzidg@o2.pl>
2025-11-06 18:36:36 +0100

    Add doctstring to function 'steps'

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,4 +1,15 @@
 def steps(number: int) -> int:
+    """Calculated how many steps are required to reach 1.
+
+    :param number: int - starting number.
+    :return: int - number of steps.
+
+    Function that returns number of steps that are required to reach 1 with
+    following rules:
+    * If number is even - divide it by 2.
+    * If number is odd - multiply by 3 and add 1.
+    """
+
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
 
