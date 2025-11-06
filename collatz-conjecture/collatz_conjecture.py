e61c6a50fd8fa3318b34abbc184efa592d3cca9a
Drahiri <kodzidg@o2.pl>
2025-11-06 18:33:49 +0100

    Restrict steps to only operate on positive integers

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,4 +1,7 @@
 def steps(number: int) -> int:
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
     counter = 0
     while number != 1:
         number = number // 2 if number % 2 == 0 else 3 * number + 1
