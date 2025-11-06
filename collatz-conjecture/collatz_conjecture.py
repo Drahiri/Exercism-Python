9ef3f21434ff614eb9f5d969ff59c20db1e814b4
Drahiri <kodzidg@o2.pl>
2025-11-06 18:26:54 +0100

    Define function 'steps'

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,7 @@
 def steps(number: int) -> int:
-    pass
+    counter = 0
+    while number != 1:
+        number = number // 2 if number % 2 == 0 else 3 * number + 1
+        counter += 1
+
+    return counter
