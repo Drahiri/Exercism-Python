637fa0550e349ddcb2fb86d79e287753ced9f759
Drahiri <kodzidg@o2.pl>
2025-11-07 11:16:30 +0100

    Add docstrings to functions

--- grains/grains.py
+++ grains/grains.py
@@ -1,4 +1,12 @@
 def square(number: int) -> int:
+    """Calculates number of grains on chessboard square.
+
+    :param number: int - square number in range 1-64.
+    :return: int - number of grains.
+
+    Function that calculates the number of grains on given square number.
+    Number of grains is twice as much as on previous square.
+    """
     if number <= 0 or number > 64:
         raise ValueError("Square must be between 1 and 64")
 
@@ -6,4 +14,9 @@ def square(number: int) -> int:
 
 
 def total() -> int:
+    """Calculates sum of grains on whole chessboard.
+
+    :return: int - sum of grains.
+    """
+
     return (1 << 64) - 1
