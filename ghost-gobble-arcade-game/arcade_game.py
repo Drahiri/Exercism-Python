a2a448c0d66fc2e1464f8bf8833ca2ef26295695
Drahiri <kodzidg@o2.pl>
2025-11-10 09:59:46 +0100

    Prepare exercise: Ghost Gobble Arcade Game

---
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -0,0 +1,45 @@
+"""Functions for implementing the rules of the classic arcade game Pac-Man"""
+
+
+def eat_ghost(power_pellet_active: bool, touching_ghost: bool) -> bool:
+    """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet.
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost: bool - is the player touching a ghost?
+    :return: bool - can a ghost be  eaten?
+    """
+    pass
+
+
+def score(touching_power_pellet: bool, touching_dot: bool) -> bool:
+    """Verify that Pac-Man has scored when a power pellet or dot has benn eaten.
+
+    :param touching_power_pellet: bool - is the player touching a power pellet?
+    :param touching_dot: bool - is the player touching a dot?
+    :param: bool - has the player scored of not?
+    """
+    pass
+
+
+def lose(power_pellet_active: bool, touching_ghost: bool) -> bool:
+    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost
+    without his power pellet.
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost: bool - is the player touching a ghost?
+    :return: bool - has the player lost the game?
+    """
+    pass
+
+
+def win(
+    has_eaten_all_dots: bool, power_pellet_active: bool, touching_ghost: bool
+) -> bool:
+    """Trigger the victory event when all dots have been eaten.
+
+    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost: bool - is the player touching a ghost?
+    :return: bool - has the player won the game?
+    """
+    pass
