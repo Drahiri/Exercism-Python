3c87e2c9089c98c80900dc825aa28a0e5eeed66d
Drahiri <kodzidg@o2.pl>
2025-11-10 09:21:37 +0100

    Define function 'square_root'

--- square-root/square_root.py
+++ square-root/square_root.py
@@ -1,2 +1,16 @@
 def square_root(number: int) -> int:
-    pass
+    """Calculate integer square root using binary search.
+
+    :param number: int - number to square root.
+    """
+    L = 0
+    R = number + 1
+
+    while L != R - 1:
+        M = (L + R) // 2
+        if M * M <= number:
+            L = M
+        else:
+            R = M
+
+    return L
