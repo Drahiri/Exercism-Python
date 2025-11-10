49264baea4e90229d3dde4d8c79dc52a73b6d4fe
Drahiri <kodzidg@o2.pl>
2025-11-10 13:17:14 +0100

    Define function 'leap_year'

--- leap-year/leap_year.py
+++ leap-year/leap_year.py
@@ -1,2 +1,7 @@
 def leap_year(year: int) -> bool:
-    pass
+    """Determines if year is a leap year.
+
+    :param year: int - year to check if its leap year.
+    :return: bool - is year a leap year?
+    """
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
