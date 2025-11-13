4b97debb108c90efba35e4db6e7e29c3fe39cd64
Drahiri <kodzidg@o2.pl>
2025-11-13 10:02:46 +0100

    Prepare exercise: Triangle

---
+++ triangle/triangle.py
@@ -0,0 +1,10 @@
+def equilateral(sides: list[int | float]) -> bool:
+    return False
+
+
+def isosceles(sides: list[int | float]) -> bool:
+    return False
+
+
+def scalene(sides: list[int | float]) -> bool:
+    return False
