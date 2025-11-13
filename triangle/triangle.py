9523980fecd6d11ae8ad4edc5811e738b8ea28db
Drahiri <kodzidg@o2.pl>
2025-11-13 10:05:08 +0100

    Define function 'equilateral'

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,5 +1,10 @@
 def equilateral(sides: list[int | float]) -> bool:
-    return False
+    """Checks if triangle is equilateral (all sides same length).
+
+    :param sides: list[int | float] - list of sides.
+    :return: bool - is triangle equilateral?
+    """
+    return sides[0] == sides[1] and sides[0] == sides[2] if sides[0] > 0 else False
 
 
 def isosceles(sides: list[int | float]) -> bool:
