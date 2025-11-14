a734b82ab969208aa442c68cef8740d4f1b4f41c
Drahiri <kodzidg@o2.pl>
2025-11-14 10:05:15 +0100

    Add docstring to 'response'

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,15 @@
-def response(hey_bob):
+def response(hey_bob: str) -> str:
+    """Bobs' response to statement.
+
+    :param hey_bob: str - statement.
+    :return: str - reply.
+
+    Reply can be one of:
+    - "Sure" - on statements ending with question mark (?).
+    - "Whoa, chill out!" - on statements with all CAPITAL LETTERS.
+    - "Calm down, I know what I'm doing!" - on statements with all
+          CAPITAL LETTERS and question mark.
+    - "Fine. Be that way!" - on empty or only whitespace statements.
+    - "Whatever" - on any other statement.
+    """
     pass
