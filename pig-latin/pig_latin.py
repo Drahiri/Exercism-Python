13697483a65c97b72a14caa802b6f593e4514450
Drahiri <kodzidg@o2.pl>
2025-11-17 10:31:28 +0100

    'translate' moves consonants at end and adds 'ay'

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,7 +1,15 @@
 def translate(text: str) -> str:
     vowels = ["a", "e", "i", "o", "u"]
 
+    # Rule 1
     if text[0] in vowels or text[:2] in ["xr", "yt"]:
         text += "ay"
+    else:
+        # Rule 2
+        index = 0
+        while text[index] not in vowels:
+            index += 1
+
+        text = text[index:] + text[:index] + "ay"
 
     return text
