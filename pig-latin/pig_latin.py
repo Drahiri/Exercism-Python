37099ed6283b4418cfad84ababc4bd9ad343c1de
Drahiri <kodzidg@o2.pl>
2025-11-17 10:19:31 +0100

    'translate' add 'ay' correctly

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,2 +1,7 @@
-def translate(text):
-    pass
+def translate(text: str) -> str:
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if text[0] in vowels or text[:2] in ["xr", "yt"]:
+        text += "ay"
+
+    return text
