0fecdd7afe60be92e4e722fc02c863dbf5baf8c0
Drahiri <kodzidg@o2.pl>
2025-11-17 10:54:15 +0100

    Treat 'y' as consonant if word starts with it

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -5,9 +5,12 @@ def translate(text: str) -> str:
     if text[0] in vowels or text[:2] in ["xr", "yt"]:
         text += "ay"
     else:
-        # Rule 2
         index = 0
-        while text[index] not in vowels:
+        # Rule 4
+        if text[0] == "y":
+            index += 1
+        # Rule 2
+        while text[index] not in [*vowels, "y"]:
             index += 1
         # Rule 3
         if text[index - 1 : index + 1] == "qu":
