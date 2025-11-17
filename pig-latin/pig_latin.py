1bb997851d3e812f83a4dd99ce55ef483a229183
Drahiri <kodzidg@o2.pl>
2025-11-17 11:07:08 +0100

    Refactor: move 'ay' suffix for vowels and 'xr', 'yt'

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -3,21 +3,17 @@ def translate(text: str) -> str:
     changed_words = []
 
     for word in text.split():
-        # Rule 1
-        if word[0] in vowels or word[:2] in ["xr", "yt"]:
-            changed_words.append(word + "ay")
-        else:
+        if not (word[0] in vowels or word[:2] in ["xr", "yt"]):
             index = 0
-            # Rule 4
             if word[0] == "y":
                 index += 1
-            # Rule 2
             while word[index] not in [*vowels, "y"]:
                 index += 1
-            # Rule 3
             if word[index - 1 : index + 1] == "qu":
                 index += 1
 
-            changed_words.append(word[index:] + word[:index] + "ay")
+            word = word[index:] + word[:index]
+
+        changed_words.append(word + "ay")
 
     return " ".join(changed_words)
