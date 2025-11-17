99082cb93d6d3ad3a7ded07e795a775320d6d11e
Drahiri <kodzidg@o2.pl>
2025-11-17 09:45:40 +0100

    Define function 'is_paired'

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,2 +1,12 @@
-def is_paired(input_string):
-    pass
+def is_paired(input_string: str) -> bool:
+    matching_bracket = {"[": "]", "{": "}", "(": ")"}
+    found_brackets = []
+
+    for c in input_string:
+        if c in matching_bracket.keys():
+            found_brackets.append(c)
+        elif c in matching_bracket.values():
+            if not found_brackets or c != matching_bracket[found_brackets.pop()]:
+                return False
+
+    return not found_brackets
