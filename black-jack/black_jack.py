6be3df829429c21d0a4bc29e1b64f287d01ae734
Drahiri <kodzidg@o2.pl>
2025-11-19 13:25:41 +0100

    Define function 'can_double_down'

--- black-jack/black_jack.py
+++ black-jack/black_jack.py
@@ -88,4 +88,4 @@ def can_double_down(card_one, card_two):
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - can the hand double down? (i.e. totals 9, 10 or 11 points).
     """
-    pass
+    return 8 < value_of_card(card_one) + value_of_card(card_two) < 12
