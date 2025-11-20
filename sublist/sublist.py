07c758c17f90b14cba2ff42ff9b68725b663457f
Drahiri <kodzidg@o2.pl>
2025-11-20 12:35:45 +0100

    Change multiple returns into one

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -5,17 +5,17 @@ SUPERLIST = 3
 
 
 def sublist(list_one: list, list_two: list) -> int:
-    if list_one == list_two:
-        return EQUAL
+    value = UNEQUAL
 
+    if list_one == list_two:
+        value = EQUAL
     elif len(list_one) > len(list_two):
         for i in range(len(list_one) - len(list_two) + 1):
             if list_one[i : i + len(list_two)] == list_two:
-                return SUPERLIST
-
+                value = SUPERLIST
     elif len(list_one) < len(list_two):
         for i in range(len(list_two) - len(list_one) + 1):
             if list_two[i : i + len(list_one)] == list_one:
-                return SUBLIST
+                value = SUBLIST
 
-    return UNEQUAL
+    return value
