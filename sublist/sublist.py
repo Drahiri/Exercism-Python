db4de306d7a659a6e1cc5259a686594042f4fa3d
Drahiri <kodzidg@o2.pl>
2025-11-20 12:33:28 +0100

    Define function 'sublist'

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -4,5 +4,18 @@ SUBLIST = 2
 SUPERLIST = 3
 
 
-def sublist(list_one, list_two):
-    pass
+def sublist(list_one: list, list_two: list) -> int:
+    if list_one == list_two:
+        return EQUAL
+
+    elif len(list_one) > len(list_two):
+        for i in range(len(list_one) - len(list_two) + 1):
+            if list_one[i : i + len(list_two)] == list_two:
+                return SUPERLIST
+
+    elif len(list_one) < len(list_two):
+        for i in range(len(list_two) - len(list_one) + 1):
+            if list_two[i : i + len(list_one)] == list_one:
+                return SUBLIST
+
+    return UNEQUAL
