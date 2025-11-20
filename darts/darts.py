c3ecb73da98f47213c9b53799c22e9263366da9c
Drahiri <kodzidg@o2.pl>
2025-11-20 10:29:58 +0100

    Define function 'score'

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,14 @@
-def score(x, y):
-    pass
+import math
+
+
+def score(x: float, y: float) -> int:
+    radius_to_points = {1: 10, 5: 5, 10: 1}  # {radius: points}
+    distance = math.sqrt(x * x + y * y)
+    scored = 0
+
+    for radius, points in radius_to_points.items():
+        if distance <= radius:
+            scored = points
+            break
+
+    return scored
