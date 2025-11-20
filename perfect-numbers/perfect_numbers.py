7b6f5260750452627fc5969e1b6607d7b80efdf8
Drahiri <kodzidg@o2.pl>
2025-11-20 11:50:55 +0100

    Define function 'classify' for perfect numbers

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,7 +1,22 @@
-def classify(number):
+import math
+
+
+def classify(number: int) -> str:
     """A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    divisors_sum = 0
+    for i in range(1, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            divisors_sum += i
+            if i != number // i:
+                divisors_sum += number // i
+    divisors_sum -= number
+
+    classification = ""
+    if divisors_sum == number:
+        classification = "perfect"
+
+    return classification
