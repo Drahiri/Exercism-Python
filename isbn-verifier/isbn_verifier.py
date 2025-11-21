589e428de631e70b19f8d3912377c6056b7f5b01
Drahiri <kodzidg@o2.pl>
2025-11-21 13:23:01 +0100

    'is_valid' correctly checks isbn

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -3,4 +3,17 @@ def is_valid(isbn: str) -> bool:
 
     if len(without_dashes) != 10:
         return False
-    return True
+
+    result = 0
+    ord_0 = ord("0")
+    ord_9 = ord("9")
+    for i, number in enumerate(without_dashes):
+        if ord_0 <= ord(number) <= ord_9:
+            result += int(number) * (10 - i)
+        else:
+            if number == "X" and i == len(without_dashes) - 1:
+                result += 10
+            else:
+                return False
+
+    return result % 11 == 0
