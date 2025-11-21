350e533c36ba45d6bd057b503f09aa73506c8673
Drahiri <kodzidg@o2.pl>
2025-11-21 09:40:53 +0100

    'rebase' function raises on wrong input/output base

--- all-your-base/all_your_base.py
+++ all-your-base/all_your_base.py
@@ -1,2 +1,3 @@
 def rebase(input_base, digits, output_base):
-    pass
+    if input_base < 2 or output_base < 2:
+        raise ValueError(f"{'input' if input_base < 2 else 'output'} base must be >= 2")
