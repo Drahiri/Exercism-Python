dab99501ba4b312dd07f713bd788fb89697b02d2
Drahiri <kodzidg@o2.pl>
2025-11-22 09:50:26 +0100

    Define function 'rotate'

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,12 @@
-def rotate(text, key):
-    pass
+def rotate(text: str, key: int) -> str:
+    encrypted = []
+    for c in text:
+        if c.isalpha():
+            if c.islower():
+                c = chr((ord(c) - 97 + key) % 26 + 97)
+            else:
+                c = chr((ord(c) - 65 + key) % 26 + 65)
+
+        encrypted.append(c)
+
+    return "".join(encrypted)
