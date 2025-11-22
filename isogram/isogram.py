7c280c7267e92f410db90d7cda8a15525e171bb5
Drahiri <kodzidg@o2.pl>
2025-11-22 08:19:18 +0100

    Add commented alternative solution for 'isogram'

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -5,3 +5,14 @@ def is_isogram(string):
     string = string.lower()
 
     return len(set(string)) == len(string)
+
+    # Alternative solution to match path where 'set' is still unknown.
+    # Swap with previous return
+
+    # letters = []
+    # for c in string:
+    #     if c in letters:
+    #         return False
+    #     else:
+    #         letters.append(c)
+    # return True
