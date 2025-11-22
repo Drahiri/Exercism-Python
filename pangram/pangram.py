595807dabf3165f8a4ef2c895d68b1ebf2a621ae
Drahiri <kodzidg@o2.pl>
2025-11-22 08:49:22 +0100

    Define function 'is_pangram'

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,20 @@
-def is_pangram(sentence):
-    pass
+def is_pangram(sentence: str) -> bool:
+    """Checks if sentence if pangram.
+
+    :param sentence: str - sentence to check.
+    :return: bool - is sentence pangram?
+    """
+    # Sanitize sentence
+    sentence = "".join(sentence.split())
+    sentence = "".join(sentence.split("-"))
+    sentence = "".join(sentence.split("_"))
+    sentence = sentence.lower()
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    found = []
+
+    for c in sentence:
+        if c not in found and c in alphabet:
+            found.append(c)
+
+    return len(found) == 26
