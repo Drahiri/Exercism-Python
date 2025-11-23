0fe7654a94b9536758829f816cb1cb848700302a
Drahiri <kodzidg@o2.pl>
2025-11-23 12:57:18 +0100

    Define function 'decode'

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -2,5 +2,14 @@ def encode(plain_text):
     pass
 
 
-def decode(ciphered_text):
-    pass
+def decode(ciphered_text: str) -> str:
+    # Remove spaces
+    ciphered_text = "".join(ciphered_text.split())
+    plain_letters = []
+    for c in ciphered_text:
+        if c.isnumeric():
+            plain_letters.append(c)
+        else:
+            plain_letters.append(chr(122 + (97 - ord(c))))
+
+    return "".join(plain_letters)
