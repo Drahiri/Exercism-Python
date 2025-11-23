da976f62f449332347f53c90b95cb2ff4eccc6a8
Drahiri <kodzidg@o2.pl>
2025-11-23 13:35:44 +0100

    Refactor: move similar code to 'change_letter'

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,15 +1,25 @@
+import string
+
+
+def change_letter(character: str) -> str:
+    if character.isnumeric():
+        return character
+    elif character.isalpha():
+        return chr(219 - ord(character))
+
+    return ""
+
+
 def encode(plain_text: str) -> str:
     plain_text = plain_text.lower()
     ciphered_text = []
     space_index = 0
 
     for c in plain_text:
-        if c.isnumeric():
-            ciphered_text.append(c)
-        elif c.isalpha():
-            ciphered_text.append(chr(219 - ord(c)))  # 219 = 97 + 122
-        else:
+        if c.isspace() or c in string.punctuation:
             continue
+
+        ciphered_text.append(change_letter(c))
 
         space_index += 1
         if space_index % 5 == 0:
@@ -23,9 +33,6 @@ def decode(ciphered_text: str) -> str:
     ciphered_text = "".join(ciphered_text.split())
     plain_letters = []
     for c in ciphered_text:
-        if c.isnumeric():
-            plain_letters.append(c)
-        else:
-            plain_letters.append(chr(219 - ord(c)))
+        plain_letters.append(change_letter(c))
 
     return "".join(plain_letters)
