f09ba1242285237fd6d0a89db64ca4af5be49d04
Drahiri <kodzidg@o2.pl>
2025-11-24 10:07:40 +0100

    Define function 'to_rna'

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,7 @@
-def to_rna(dna_strand):
-    pass
+def to_rna(dna_strand: str) -> str:
+    """Translate DNA strand into RNA strand.
+
+    :param dna_strand: str - dna strand to translate.
+    :return: str - rna strand.
+    """
+    return dna_strand.translate(str.maketrans("GCTA", "CGAU"))
