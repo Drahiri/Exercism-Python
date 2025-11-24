d5a2fcf8b7f711732b3dc1ba92fc5365cd459a40
Drahiri <kodzidg@o2.pl>
2025-11-24 11:24:10 +0100

    Move into 'try-except' block

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -10,22 +10,25 @@ def answer(question: str) -> int | float:
 
     result = 0
     while len(words) > 1:
-        number1 = int(words[0])
-        number2 = int(words[2])
-        operator = words[1]
-        remaining = words[3:]
+        try:
+            number1 = int(words[0])
+            number2 = int(words[2])
+            operator = words[1]
+            remaining = words[3:]
 
-        result = number1
-        match operator:
-            case "plus":
-                result += number2
-            case "minus":
-                result -= number2
-            case "multiplied":
-                result *= number2
-            case "divided":
-                result /= number2
+            result = number1
+            match operator:
+                case "plus":
+                    result += number2
+                case "minus":
+                    result -= number2
+                case "multiplied":
+                    result *= number2
+                case "divided":
+                    result /= number2
 
-        words = [result, *remaining]
+            words = [result, *remaining]
+        except IndexError:
+            raise ValueError("syntax error")
 
     return result
