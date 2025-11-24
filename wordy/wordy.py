292eab32b8d4d02a96535c09f7ee6df8ff5db063
Drahiri <kodzidg@o2.pl>
2025-11-24 11:18:17 +0100

    'answer' can solve multiple oprations

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -8,20 +8,24 @@ def answer(question: str) -> int | float:
     if len(words) == 1:
         return int(words[0])
 
-    number1 = int(words[0])
-    number2 = int(words[2])
-    operator = words[1]
-    words = words[3:]
+    result = 0
+    while len(words) > 1:
+        number1 = int(words[0])
+        number2 = int(words[2])
+        operator = words[1]
+        remaining = words[3:]
 
-    result = number1
-    match operator:
-        case "plus":
-            result += number2
-        case "minus":
-            result -= number2
-        case "multiplied":
-            result *= number2
-        case "divided":
-            result /= number2
+        result = number1
+        match operator:
+            case "plus":
+                result += number2
+            case "minus":
+                result -= number2
+            case "multiplied":
+                result *= number2
+            case "divided":
+                result /= number2
+
+        words = [result, *remaining]
 
     return result
