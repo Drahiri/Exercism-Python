2472b2116415f89663993cc541973a8cb1407ef7
Drahiri <kodzidg@o2.pl>
2025-11-24 11:02:03 +0100

    'answer' correctly inteprets questions with one number

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -1,2 +1,9 @@
-def answer(question):
-    pass
+def answer(question: str) -> int:
+    # Remove 'What is' and '?'
+    question = question.removeprefix("What is ").removesuffix("?")
+    words = question.split()
+
+    if len(words) == 1:
+        return int(words[0])
+
+    return 0
