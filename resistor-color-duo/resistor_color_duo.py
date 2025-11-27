fe5d708761d287479c172046c4c6cb70569406a5
Drahiri <kodzidg@o2.pl>
2025-11-27 12:18:31 +0100

    Define function 'value'

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,21 @@
-def value(colors):
-    pass
+COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def value(colors: list[str]) -> int:
+    """Checks value of 2 resistors colors.
+
+    :param colors: list[str] - list of colors on resistor.
+    :return: int - value of 2 resistor colors.
+    """
+    return COLORS.index(colors[0]) * 10 + COLORS.index(colors[1])
