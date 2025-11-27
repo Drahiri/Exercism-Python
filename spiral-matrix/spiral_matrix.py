6c216e74c5c567e2e45ba58e8c41113a84327972
Drahiri <kodzidg@o2.pl>
2025-11-27 12:58:33 +0100

    Define function 'spiral_matrix'

--- spiral-matrix/spiral_matrix.py
+++ spiral-matrix/spiral_matrix.py
@@ -1,2 +1,25 @@
-def spiral_matrix(size):
-    pass
+def spiral_matrix(size: int) -> list[list[int]]:
+    """Create matrix of 'size' where nubmers spirals inside.
+
+    :param size: int - size of the matrix.
+    :param: list[list[int]] - matrix of spiralling numbers.
+    """
+
+    spiral = [[0 for i in range(size)] for j in range(size)]
+
+    dx, dy = (1, 0)
+    x, y = (0, 0)
+
+    for i in range(size * size):
+        spiral[y][x] = i + 1
+
+        if (
+            any((x + dx >= size, y - dy >= size, y - dy < 0, x + dx < 0))
+            or spiral[y - dy][x + dx]
+        ):
+            dx, dy = dy, -dx
+
+        x += dx
+        y -= dy
+
+    return spiral
