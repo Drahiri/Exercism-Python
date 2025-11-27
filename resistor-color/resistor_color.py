aa2fcf9ebe79740fd4f5ff10aa23442492ccb0b0
Drahiri <kodzidg@o2.pl>
2025-11-27 12:07:39 +0100

    Add docstrings to 'color_code', 'colors'

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -13,8 +13,17 @@ COLORS = [
 
 
 def color_code(color: str) -> int:
+    """Checks value of the color.
+
+    :param color: str - name of the color.
+    :return: int - value of the color.
+    """
     return COLORS.index(color)
 
 
 def colors() -> list[str]:
+    """Lists resistor colors.
+
+    :return: list[str] - list of color names.
+    """
     return COLORS
