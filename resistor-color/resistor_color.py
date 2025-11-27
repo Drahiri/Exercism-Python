2948d7b9e4cb9594c27361ab3c9975ec3b1e210d
Drahiri <kodzidg@o2.pl>
2025-11-27 12:03:07 +0100

    Define constant 'COLORS'

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,3 +1,17 @@
+COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
 def color_code(color):
     pass
 
