ebd0612d806e823bd880698b8f5e31bfa50442a3
Drahiri <kodzidg@o2.pl>
2025-11-27 12:01:01 +0100

    Prepare exercise: Resistor Color

---
+++ resistor-color/resistor_color_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from resistor_color import color_code, colors
+
+
+class ResistorColorTest(unittest.TestCase):
+    def test_black(self):
+        self.assertEqual(color_code("black"), 0)
+
+    def test_white(self):
+        self.assertEqual(color_code("white"), 9)
+
+    def test_orange(self):
+        self.assertEqual(color_code("orange"), 3)
+
+    def test_colors(self):
+        expected = [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ]
+        self.assertEqual(colors(), expected)
