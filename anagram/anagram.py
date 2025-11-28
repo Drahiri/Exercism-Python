4fa4d238f9769ccf2d4564c5093b8a337cf9296d
Drahiri <kodzidg@o2.pl>
2025-11-28 11:38:24 +0100

    Define function 'find_anagrams'

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,22 @@
-def find_anagrams(word, candidates):
-    pass
+def find_anagrams(word: str, candidates: list[str]) -> list[str]:
+    """Find all anagrams from candidates.
+
+    :param word: str - base word for anagram.
+    :param candidates: list - possible anagrams of the word.
+    :return: list - list of anagrams from candidates.
+    """
+    result = []
+    word = word.lower()
+
+    for candidate in candidates:
+        lower_candidate = candidate.lower()
+        if len(lower_candidate) != len(word) or word == lower_candidate:
+            continue
+
+        word_list = sorted(list(word))
+        candidate_list = sorted(list(lower_candidate))
+
+        if word_list == candidate_list:
+            result.append(candidate)
+
+    return result
