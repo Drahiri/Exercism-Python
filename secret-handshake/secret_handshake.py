b565554b9766b5e1bf1c97933aca076199073f69
Drahiri <kodzidg@o2.pl>
2025-11-29 13:17:33 +0100

    Define function 'commands'

--- secret-handshake/secret_handshake.py
+++ secret-handshake/secret_handshake.py
@@ -1,2 +1,18 @@
-def commands(binary_str):
-    pass
+COMMANDS = ["wink", "double blink", "close your eyes", "jump"]
+
+
+def commands(binary_str: str) -> list[str]:
+    """Translate binary into commands.
+
+    :param binary_str: str - string of binary.
+    :return: list[str] - list of commands to perform.
+    """
+    cmds = []
+    for i in range(len(binary_str) - 1):
+        if binary_str[-1 - i] == "1":
+            cmds.append(COMMANDS[i])
+
+    if binary_str[0] == "1":
+        cmds.reverse()
+
+    return cmds
