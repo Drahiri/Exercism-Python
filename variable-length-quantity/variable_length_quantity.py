417e38547cba518e484e36d4b7b01c0cf6e77d70
Drahiri <kodzidg@o2.pl>
2025-12-01 11:12:50 +0100

    Define function 'encode'

--- variable-length-quantity/variable_length_quantity.py
+++ variable-length-quantity/variable_length_quantity.py
@@ -1,5 +1,28 @@
-def encode(numbers):
-    pass
+def encode(numbers: list[int]) -> list[int]:
+    """Encodes numbers into variable length quantity.
+
+    :param numbers: list[int] - list of numbers to encode.
+    :return: list[int] - encoded numbers.
+    """
+    payload = []
+
+    for number in numbers:
+        buffer = number & 0x7F
+        number = number >> 7
+        while number > 0:
+            buffer = buffer << 8
+            buffer |= 0x80
+            buffer += number & 0x7F
+            number = number >> 7
+
+        while True:
+            payload.append(buffer & 0xFF)
+            if buffer & 0x80:
+                buffer = buffer >> 8
+            else:
+                break
+
+    return payload
 
 
 def decode(bytes_):
