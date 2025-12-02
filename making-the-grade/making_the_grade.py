bc8057591617dc63fdfae4e621d44041bf322668
Drahiri <kodzidg@o2.pl>
2025-12-02 11:56:12 +0100

    Define function 'round_scores'

--- making-the-grade/making_the_grade.py
+++ making-the-grade/making_the_grade.py
@@ -1,14 +1,13 @@
 """Functions for organizing and calculating student exam scores."""
 
 
-def round_scores(student_scores):
+def round_scores(student_scores: list[float]) -> list[int]:
     """Round all provided student scores.
 
     :param student_scores: list - float or int of student exam scores.
     :return: list - student scores *rounded* to nearest integer value.
     """
-
-    pass
+    return [round(score) for score in student_scores]
 
 
 def count_failed_students(student_scores):
