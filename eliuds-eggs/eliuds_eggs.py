3fcc06d035de38878058e41583950434c63fe41a
Drahiri <kodzidg@o2.pl>
2025-12-03 12:18:32 +0100

    Define function 'egg_count'

--- eliuds-eggs/eliuds_eggs.py
+++ eliuds-eggs/eliuds_eggs.py
@@ -1,2 +1,14 @@
-def egg_count(display_value):
-    pass
+def egg_count(display_value: int) -> int:
+    """Shows actual egg count from displayed name.
+
+    :param display_value: int - displayed value.
+    :return: int - actual count of eggs.
+    """
+    counter = 0
+
+    while display_value > 0:
+        if display_value & 1:
+            counter += 1
+        display_value = display_value >> 1
+
+    return counter
