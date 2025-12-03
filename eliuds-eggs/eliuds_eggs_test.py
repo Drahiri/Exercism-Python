18c396ff83e627990f5dba0a28c1e297f40ed4fa
Drahiri <kodzidg@o2.pl>
2025-12-03 12:13:48 +0100

    Prepare exercise: Eliud's Eggs

---
+++ eliuds-eggs/eliuds_eggs_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+from eliuds_eggs import (
+    egg_count,
+)
+
+
+class EliudsEggsTest(unittest.TestCase):
+    def test_0_eggs(self):
+        expected = 0
+        self.assertEqual(egg_count(0), expected)
+
+    def test_1_egg(self):
+        expected = 1
+        self.assertEqual(egg_count(16), expected)
+
+    def test_4_eggs(self):
+        expected = 4
+        self.assertEqual(egg_count(89), expected)
+
+    def test_13_eggs(self):
+        expected = 13
+        self.assertEqual(egg_count(2000000000), expected)
