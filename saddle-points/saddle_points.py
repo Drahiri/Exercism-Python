386e339665030d1e030563d66b96a7c03e6cba93
Drahiri <kodzidg@o2.pl>
2025-12-04 13:09:10 +0100

    Define function Saddle Points

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,2 +1,19 @@
-def saddle_points(matrix):
-    pass
+def saddle_points(matrix: list[list[int]]) -> list[dict[str, int]]:
+    """Checks for possible location for tree house.
+
+    :param matrix: list[list[int]] - matrix of hights of trees.
+    :return: list[dict[str, int]] - list of possible row/column locations.
+    """
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise ValueError("irregular matrix")
+
+    highest_trees = list(map(max, matrix))
+    lowest_trees = list(map(min, list(zip(*matrix))))
+
+    return [
+        {"row": i + 1, "column": j + 1}
+        for i, h_tree in enumerate(highest_trees)
+        for j, l_tree in enumerate(lowest_trees)
+        if h_tree == l_tree
+    ]
